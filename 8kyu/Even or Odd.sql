9bada4b7c4ac9521e3b03371fb02e35d0df1f15b
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-10-14 05:01:27 +0500

    feat: 8 kyu solutions

---
+++ 8kyu/Even or Odd.sql
@@ -0,0 +1,11 @@
+-- -----------------------------------------------------------
+-- Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+-- -----------------------------------------------------------
+
+SELECT 
+  CASE
+    WHEN number % 2 = 0 THEN 'Even'
+    ELSE 'Odd'
+  END
+AS is_even
+FROM numbers
