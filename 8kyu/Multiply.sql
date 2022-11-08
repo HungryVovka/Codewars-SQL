3c76717f75a1845903f17dc12dc8307acafbdff5
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-11-08 23:35:14 +0500

    feat: 8 kyu solutions

--- 8kyu/Multiply.sql
+++ 8kyu/Multiply.sql
@@ -2,4 +2,4 @@
 -- This code does not execute properly. Try to figure out why.
 -- -----------------------------------------------------------
 
-SELECT price * amount AS total FROM items
+SELECT price * amount AS total FROM items;
