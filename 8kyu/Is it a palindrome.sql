8ac1f33dc32a0010486b0a879ad5fe4409381fdc
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-11-17 23:08:21 +0500

    feat: 8 kyu solutions

--- 8kyu/Is it a palindrome.sql
+++ 8kyu/Is it a palindrome.sql
@@ -2,4 +2,8 @@
 -- Write a function that checks if a given string (case insensitive) is a palindrome.
 -- -----------------------------------------------------------
 
+-- # write your SQL statement here: you are given a table 'ispalindrome' 
+-- with column 'str', return a table with column 'str' and your result 
+-- in a column named 'res'.
+
 SELECT str, LOWER(str) = REVERSE(LOWER(str)) AS res FROM ispalindrome;
