4221914acef7c6bca0833c5f52d0038dc6616893
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-11-17 23:10:56 +0500

    feat: 8 kyu solutions

--- 8kyu/Will you make it.sql
+++ 8kyu/Will you make it.sql
@@ -7,4 +7,8 @@
 -- Function should return true if it is possible and false if not.
 -- -----------------------------------------------------------
 
+--# write your SQL statement here: you are given a table 'zerofuel' 
+-- with columns 'distance_to_pump', 'mpg' and 'fuel_left', return 
+-- a table with all the columns and your result in a column named 'res'.
+
 SELECT distance_to_pump, mpg, fuel_left, distance_to_pump <= fuel_left * mpg AS res FROM zerofuel;
