e231cbb1740bd4eff4409d68b958c37748101665
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-11-17 23:14:12 +0500

    feat: 8 kyu solutions

--- 8kyu/String repeat.sql
+++ 8kyu/String repeat.sql
@@ -6,4 +6,8 @@
 -- 5, "Hello" -> "HelloHelloHelloHelloHello"
 -- -----------------------------------------------------------
 
+--# write your SQL statement here: you are given a table 'repeatstr' 
+-- with columns 'n' and 's', return a table with all columns 
+-- and your result in a column named 'res'.
+
 SELECT s, n, REPEAT(s, n) AS res FROM repeatstr;
