e01adb3146db825476638045be11a55fc538e84b
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-11-17 23:20:58 +0500

    feat: 8 kyu solutions

--- 8kyu/Opposite number.sql
+++ 8kyu/Opposite number.sql
@@ -10,4 +10,7 @@
 -- You will be given a table: opposite, with a column: number. Return a table with a column: res.
 -- -----------------------------------------------------------
 
+-- You will be given a table: "opposite", with a column: "number". 
+-- Return a table with a column: "res".
+
 SELECT - number AS res FROM opposite;
