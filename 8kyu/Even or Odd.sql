314c7e8ba777508188dadb3bb823a18ac011f2d8
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-11-19 23:24:55 +0500

    fix: 8 kyu solutions

--- 8kyu/Even or Odd.sql
+++
@@ -1,5 +0,0 @@
--- -----------------------------------------------------------
--- Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
--- -----------------------------------------------------------
-
-SELECT CASE WHEN number % 2 = 0 THEN 'Even' ELSE 'Odd' END AS is_even FROM numbers;
--- 8kyu/Remove exclamation marks.sql
+++
@@ -1,9 +0,0 @@
--- -----------------------------------------------------------
--- Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
--- -----------------------------------------------------------
-
--- # write your SQL statement here: you are given a table 
--- 'removeexclamationmarks' with column 's', 
--- return a table with column 's' and your result in a column named 'res'.
-
-SELECT s, REPLACE(s, '!', '') AS res FROM removeexclamationmarks;
