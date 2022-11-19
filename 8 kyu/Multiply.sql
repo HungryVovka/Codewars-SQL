664fbabb3f8a0a24aa1c1115e066ddc849098271
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-11-19 23:24:10 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Multiply.sql
@@ -0,0 +1,5 @@
+-- -----------------------------------------------------------
+-- This code does not execute properly. Try to figure out why.
+-- -----------------------------------------------------------
+
+SELECT price * amount AS total FROM items
