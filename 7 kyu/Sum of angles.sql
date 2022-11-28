b7b81c1ee5e33834935c351a2fbe62cc74fa548b
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-11-28 23:04:32 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/Sum of angles.sql
@@ -0,0 +1,5 @@
+-- -----------------------------------------------------------
+-- Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+-- -----------------------------------------------------------
+
+SELECT (n - 2) * 180 AS res FROM angle;
