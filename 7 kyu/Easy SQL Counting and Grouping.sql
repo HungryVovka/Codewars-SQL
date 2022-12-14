61f2d984f39b10127dea193eb7cc1a3162cb034c
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-12-15 00:30:21 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/Easy SQL Counting and Grouping.sql
@@ -0,0 +1,20 @@
+-- -----------------------------------------------------------
+-- Given a demographics table in the following format:
+-- 
+-- ** demographics table schema **
+-- 
+-- id
+-- name
+-- birthday
+-- race
+-- 
+-- you need to return a table that shows a count of each race represented, ordered by the count in descending order as:
+-- 
+-- ** output table schema **
+-- 
+-- race
+-- count
+-- -----------------------------------------------------------
+
+SELECT race, COUNT(race) FROM demographics
+GROUP BY race ORDER BY COUNT(race) DESC;
---
+++ 7 kyu/SQL Basics - Trimming the Field.sql
@@ -0,0 +1,33 @@
+-- -----------------------------------------------------------
+-- You have access to a table of monsters as follows:
+-- 
+-- monsters schema
+-- 
+-- id
+-- name
+-- legs
+-- arms
+-- characteristics
+-- 
+-- The monsters in the provided table have too many characteristics, they really only need one each. Your job is to trim the characteristics down so that 
+-- each monster only has one. If there is only one already, provide that. If there are multiple, provide only the first one (don't leave any commas in 
+-- there).
+-- 
+-- You must return a table with the format as follows:
+-- 
+-- output schema
+-- 
+-- id
+-- name
+-- characteristic
+-- 
+-- Order by id
+-- -----------------------------------------------------------
+
+SELECT id, name, SPLIT_PART(characteristics, ',', 1) AS characteristic
+FROM monsters ORDER BY id;
+
+-- or
+
+SELECT id, name, SUBSTRING(characteristics, '^[a-z]+') AS characteristic
+FROM monsters ORDER BY id;
