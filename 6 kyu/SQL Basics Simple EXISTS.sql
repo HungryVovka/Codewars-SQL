275bedd914f44b4ebc0fba98262b41f42698afe8
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-01-05 23:52:08 +0500

    feat: 6 kyu solutions

---
+++ 6 kyu/SQL Basics Simple EXISTS.sql
@@ -0,0 +1,28 @@
+-- -----------------------------------------------------------
+-- For this challenge you need to create a SELECT statement that will contain data about departments that had a sale with a price over 98.00 dollars. 
+-- This SELECT statement will have to use an EXISTS to achieve the task.
+-- 
+-- departments table schema
+-- id
+-- name
+-- 
+-- sales table schema
+-- id
+-- department_id (department foreign key)
+-- name
+-- price
+-- card_name
+-- card_number
+-- transaction_date
+-- 
+-- resultant table schema
+-- id
+-- name
+-- -----------------------------------------------------------
+
+-- Create your SELECT statement here
+
+SELECT id, name FROM departments
+WHERE EXISTS(SELECT * FROM sales
+             WHERE sales.department_id = departments.id AND
+                   sales.price > '98.00')
