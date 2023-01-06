3e5963e269e640f1a4b62a1ed27292ff1c5c4369
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-01-06 23:23:55 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/GROCERY STORE Support Local Products.sql
@@ -0,0 +1,30 @@
+-- -----------------------------------------------------------
+-- You are the owner of the Grocery Store. All your products are in the database, that you have created after CodeWars SQL excercises!:)
+-- 
+-- You care about local market, and want to check how many products come from United States of America or Canada.
+-- 
+-- Please use SELECT statement and IN to filter out other origins.
+-- 
+-- In the results show how many products are from United States of America and Canada respectively.
+-- 
+-- Order by number of products, descending.
+-- 
+-- products table schema
+-- id
+-- name
+-- price
+-- stock
+-- producer
+-- country
+-- 
+-- results table schema
+-- products
+-- country
+-- -----------------------------------------------------------
+
+-- Replace with your SQL Query
+
+SELECT COUNT(name) AS products, country
+FROM products
+WHERE country IN ('Canada', 'United States of America')
+GROUP BY country ORDER BY products DESC;
