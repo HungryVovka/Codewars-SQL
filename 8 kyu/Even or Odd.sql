5edbbd0ec92047db8fd0ec40940999f879b0c767
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-03-26 03:37:27 +0500

    feat: 8 kyu solutions

--- 8 kyu/Even or Odd.sql
+++ 8 kyu/Even or Odd.sql
@@ -1,5 +1,20 @@
 -- -----------------------------------------------------------
--- Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+-- You will be given a table numbers, with one column number.
+-- 
+-- Return a dataset with two columns: number and is_even, where number contains the original input value, and is_even containing "Even" or 
+-- "Odd" depending on number column values.
+-- 
+-- Numbers table schema
+-- * number INT
+-- 
+-- Output table schema
+-- * number  INT
+-- * is_even STRING
 -- -----------------------------------------------------------
 
-SELECT CASE WHEN number % 2 = 0 THEN 'Even' ELSE 'Odd' END AS is_even FROM numbers
+SELECT number,
+CASE 
+    WHEN number % 2 = 0 THEN 'Even' 
+    ELSE 'Odd'
+END 
+AS is_even FROM numbers;
