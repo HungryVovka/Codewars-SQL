78a4ff4e6821b23ec86e7c87747e8afec456fc9c
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-04-06 02:05:51 +0500

    feat: 5 kyu solutions

--- 5 kyu/SQL Basics Simple PIVOTING data.sql
+++ 5 kyu/SQL Basics Simple PIVOTING data.sql
@@ -7,7 +7,7 @@
 -- 
 -- id 				id
 -- name 			product_id
--- 				detail
+-- 					detail
 --  
 -- products table schema
 -- - id   - integer
@@ -54,3 +54,24 @@ crosstab.bad FROM crosstab(
     $$)
 AS crosstab (name TEXT, ok INTEGER, good INTEGER, bad INTEGER)
 ORDER BY name;
+
+-- -----------------------------------------------------------
+-- License
+-- Tasks are the property of Codewars (https://www.codewars.com/) 
+-- and users of this resource.
+-- 
+-- All solution code in this repository 
+-- is the personal property of Vladimir Rukavishnikov
+-- (vladimirrukavishnikovmail@gmail.com).
+-- 
+-- Copyright (C) 2022 Vladimir Rukavishnikov
+-- 
+-- This file is part of the HungryVovka/Codewars-SQL
+-- (https://github.com/HungryVovka/Codewars-SQL)
+-- 
+-- License is GNU General Public License v3.0
+-- (https://github.com/HungryVovka/Codewars-SQL/blob/main/LICENSE)
+-- 
+-- You should have received a copy of the GNU General Public License v3.0
+-- along with this code. If not, see http://www.gnu.org/licenses/
+-- -----------------------------------------------------------
